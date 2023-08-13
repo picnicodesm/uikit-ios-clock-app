89713340bcbfe04e923a69e93a0e2f1c4c1a4dd5
Sangmin Kim <offiksm0903@naver.com>
2023-08-13 21:05:21 +0900

    Initial Commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by 김상민 on 2023/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
